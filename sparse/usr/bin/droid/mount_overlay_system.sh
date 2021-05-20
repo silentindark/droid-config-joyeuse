6ad21950244602c3b03372d33b010981a195d839
Vlad G <b100dian@gmail.com>
2021-05-20 13:21:27 +0300

    No overlay, no crash
    Change-Id: Ie3b6c7bc0fd7e5f11788426664a5a8955848c9a9

--- sparse/usr/bin/droid/mount_overlay_system.sh
+++ sparse/usr/bin/droid/mount_overlay_system.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-mkdir -p /system
-mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/system:/system_root/system /system
- 
+#mkdir -p /system
+#mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/system:/system_root/system /system
+
+# Disabled overall overay because it crashes graphics composer. Manually picking:
+
+mount --bind /usr/libexec/droid-hybris/system/lib64/libprocessgroup_setup.so /system/lib64/libprocessgroup_setup.so 
+mkdir -p /persist
+mount --bind /mnt/vendor/persist /persist
